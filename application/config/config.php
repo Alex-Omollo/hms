81f548bfb9fc53cce31971d29f759986f25f079e
samarai <71961064+Alex-Omollo@users.noreply.github.com>
2024-10-24 10:04:06 +0300

    /config config.php - 17

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/hms/';
+$config['base_url']	= 'http://192.168.100.72:41062/hms/';
 
 /*
 |--------------------------------------------------------------------------
